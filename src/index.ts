18ee762fc5d4b7f361ce6b3ced73fb48faa4f61e
Karol Wolski <karolwolski04@gmail.com>
2022-07-06 13:44:06 +0200

    feat: Add Player - Human

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import ComputerPlayer from './js/ComputerPlayer'
+import HumanPlayer from './js/HumanPlayer'
 import './styles/style.scss'
+
+const init = () => {
+  const buttons = document.querySelectorAll('.btn__symbol')
+  let humanPlayer
+  const computerPlayer = ComputerPlayer()
+
+  buttons.forEach(button =>
+    button.addEventListener('click', () => {
+      humanPlayer = HumanPlayer(button.getAttribute('data-symbol'))
+      console.log(humanPlayer, computerPlayer)
+    }),
+  )
+}
+
+init()
