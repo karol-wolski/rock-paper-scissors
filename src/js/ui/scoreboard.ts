5a88ee4b74b59f6a30b9f6523af0624a667ae2e5
Karol Wolski <karolwolski04@gmail.com>
2022-07-11 12:52:53 +0200

    feat: player computer mode (#22)

---
+++ src/js/ui/scoreboard.ts
@@ -0,0 +1,11 @@
+const scoreboard = () => {
+  return `
+    <div class="scoreboard">
+      <p class="scoreboard__team">Player</p>
+      <p id="score" class="scoreboard__score">0 : 0</p>
+      <p class="scoreboard__team">Computer</p>
+    </div>
+  `
+}
+
+export default scoreboard
