d96fe5980b15f17be932911aa401f4044c9f7fba
Karol Wolski <karolwolski04@gmail.com>
2022-07-12 12:36:11 +0200

    Revert "feat: player computer mode (#22)"
    This reverts commit 5a88ee4b74b59f6a30b9f6523af0624a667ae2e5.

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,15 @@ import { COMPUTER_PLAYER, HUMAN_PLAYER } from './js/constants'
 import displayResult from './js/displayResult'
 import displaySymbols from './js/displaySymbols'
 import HumanPlayer from './js/HumanPlayer'
-import arena from './js/ui/arena'
-import scoreboard from './js/ui/scoreboard'
-import buttonsWithSymbols from './js/ui/buttons-with-symbols'
 import './styles/style.scss'
-import startMenu from './js/ui/startMenu'
-import backToTheMenu from './js/ui/backToTheMenu'
 
-const playerComputerMode = () => {
+const init = () => {
   const buttons = document.querySelectorAll('.btn-symbol')
-  const main = document.querySelector('.main')
-  main.innerHTML = scoreboard()
-  main.innerHTML += arena()
-  main.innerHTML += buttonsWithSymbols()
-  backToTheMenu()
   const result = {
     human: 0,
     computer: 0,
   }
+
   buttons.forEach(button =>
     button.addEventListener('click', () => {
       const humanPlayer = HumanPlayer(button.getAttribute('data-symbol'))
@@ -33,13 +24,6 @@ const playerComputerMode = () => {
       displayResult(result.human, result.computer)
     }),
   )
-}
-
-const init = () => {
-  const main = document.querySelector('.main')
-  main.innerHTML += startMenu()
-  const playerComputerModeBtn = document.querySelector('#player-computer-mode')
-  playerComputerModeBtn.addEventListener('click', () => playerComputerMode())
 }
 
 init()
